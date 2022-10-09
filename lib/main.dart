def953cbc8f6e2d2c4ba28210ebf8d8fea6b9002
Serg8787 <tsybulnikserg@gmail.com>
2022-10-09 13:22:26 +0200

    add main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medition_app/screens/medition_app_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
 
-      home:
+      home: MeditionScreen(),
     );
   }
 }
---
+++ lib/screens/medition_app_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MeditionScreen extends StatefulWidget {
+  @override
+  State<MeditionScreen> createState() => _MeditionScreenState();
+}
+
+class _MeditionScreenState extends State<MeditionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
