2ae30da45b4d405b798acfef24c7b22fe859620d
Serg8787 <tsybulnikserg@gmail.com>
2022-10-10 17:31:55 +0200

    add list_view

--- lib/model/item_model.dart
+++ lib/model/item_model.dart
@@ -1,8 +1,8 @@
-class Item {
+class ItemMeditiion {
   final String name;
   final String audioPath;
   final String imagePath;
 
 
-  Item({required this.name,required this.audioPath,required this.imagePath});
+  ItemMeditiion({required this.name,required this.audioPath,required this.imagePath});
 }
--- lib/screens/medition_app_screen.dart
+++ lib/screens/medition_app_screen.dart
@@ -7,19 +7,54 @@ class MeditionScreen extends StatefulWidget {
 }
 
 class _MeditionScreenState extends State<MeditionScreen> {
-  
-  final Item item = Item(name: "Forest", audioPath: "medition_audio/forest.mp3", imagePath: "meditions_images/forest.jpeg");
-  final Item item1 = Item(name: "Night", audioPath: "medition_audio/night.mp3", imagePath: "meditions_images/night.jpeg");
-  final Item item2 = Item(name: "Ocean", audioPath: "medition_audio/ocean.mp3", imagePath: "meditions_images/ocean.jpeg");
-  final Item item3 = Item(name: "Waterwall", audioPath: "medition_audio/waterwall.mp3",
-      imagePath: "meditions_images/waterwall.jpeg");
-  final Item item4 = Item(name: "Wid", audioPath: "medition_audio/wind.mp3", imagePath: "meditions_images/wind.jpeg");
+
+  final List<ItemMeditiion> meditions = [
+    ItemMeditiion(
+        name: "Forest",
+        audioPath: "medition_audio/forest.mp3",
+        imagePath: "meditions_images/forest.jpeg"),
+    ItemMeditiion(
+        name: "Night",
+        audioPath: "medition_audio/night.mp3",
+        imagePath: "meditions_images/night.jpeg"),
+    ItemMeditiion(
+        name: "Ocean",
+        audioPath: "medition_audio/ocean.mp3",
+        imagePath: "meditions_images/ocean.jpeg"),
+    ItemMeditiion(
+        name: "Waterwall",
+        audioPath: "medition_audio/waterwall.mp3",
+        imagePath: "meditions_images/waterwall.jpeg"),
+    ItemMeditiion(
+        name: "Wind",
+        audioPath: "medition_audio/wind.mp3",
+        imagePath: "meditions_images/wind.jpeg")
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: ListView(
+          physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          children: [
+            Container(
+              height: 50,
+              color: Colors.orange,
+              child: Text(meditions[0].name),
+            ),
+            Container(
+              height: 50,
+              color: Colors.green,
+              child: Text(meditions[1].name),
+            ),
+            Container(
+              height: 50,
+              color: Colors.blue,
+              child: Text(meditions[2].name),
+            ),
+          ],
+        ),
       ),
     );
   }
